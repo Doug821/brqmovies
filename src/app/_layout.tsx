99798f3478c3ee5295e8aabf6c65f550830e3a4f
Herson Douglas <dougsilva821@gmail.com>
2024-08-07 08:05:11 -0300

    chore: Update layout component to include status bar and safe area view

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,17 +2,21 @@ import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
 import { AppProvider } from '@/contexts';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native';
 
 export { ErrorBoundary } from 'expo-router';
 
 export default function RootLayout() {
-
   return (
     <AppProvider>
-      <Stack initialRouteName='signIn'>
-        <Stack.Screen name='signIn' options={{ headerShown: false }} />
-        <Stack.Screen name='(app)' options={{ headerShown: false }} />
-      </Stack>
+      <StatusBar style='light' />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack initialRouteName='signIn'>
+          <Stack.Screen name='signIn' options={{ headerShown: false }} />
+          <Stack.Screen name='(app)' options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
     </AppProvider>
   );
 }
