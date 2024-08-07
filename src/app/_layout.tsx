3d598483f7b8feb6f6514bb2cfb30f7749e05f94
Herson Douglas <dougsilva821@gmail.com>
2024-08-07 13:52:50 -0300

    refactor: Update _layout.tsx to remove SafeAreaView
    This commit removes the SafeAreaView component from the _layout.tsx file. The SafeAreaView was previously used to ensure that the content is displayed within the safe area boundaries of the device. However, it is no longer necessary as the Stack component from expo-router already handles safe area insets. This change simplifies the code and improves performance.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Stack } from 'expo-router';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import 'react-native-reanimated';
 
 import { AppProvider } from '@/contexts';
@@ -9,13 +9,11 @@ export { ErrorBoundary } from 'expo-router';
 export default function RootLayout() {
   return (
     <AppProvider>
-      <StatusBar backgroundColor='#16171B' />
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack initialRouteName='signIn'>
+        <StatusBar backgroundColor='#16171B' barStyle={'light-content'} />
+        <Stack initialRouteName='(app)'>
           <Stack.Screen name='signIn' options={{ headerShown: false }} />
           <Stack.Screen name='(app)' options={{ headerShown: false }} />
         </Stack>
-      </SafeAreaView>
     </AppProvider>
   );
 }
